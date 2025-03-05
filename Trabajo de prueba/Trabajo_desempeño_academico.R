d68893f9ff403b0a13d6a0169323ed335dc73aa4
DavidMazza02 <davidmazzarequena@gmail.com>
2025-03-05 19:46:07 -0400

    Cálculo de simetria, kurtosis con gráfico
    Se realizó los calculos de simetria, kurtosis de la variable Desempeño y se modificó el tipo de fuentes de los graficos previos

--- Trabajo de prueba/Trabajo_desempeño_academico.R
+++ Trabajo de prueba/Trabajo_desempeño_academico.R
@@ -3,6 +3,7 @@ library(readxl)
 library(tidyverse)
 library(ggplot2)
 library(knitr)
+library(e1071)
 
 #Lectura de base de datos
 SP <- read_excel("C:/Users/PC/Documents/GitHub/Statistical-analysis-on-student-performance-/StudentsPerformance G5-G6.xlsx")
@@ -29,13 +30,51 @@ Calculos_simples <- notas %>%
     Mediana = round(median(Desempeño), 2),
     Varianza = round(var(Desempeño), 2),
     Desviacion_estandar = round(sd(Desempeño), 2),
-    Coeficiente_de_variacion = paste0(round(((Desviacion_estandar / Promedio) * 100), 2), "%")
+    Coeficiente_de_variacion = paste0(round(((Desviacion_estandar / Promedio) * 100), 2), "%"),
+    Kurtosis = round(kurtosis(Desempeño), 2),
+    Simetria = round(skewness(Desempeño), 2)
   )
+
 
 kable(calculos_simples,
       align = "c", 
       caption = "Cálculo de medidas descriptivas")
-  
+
+
+
+
+
+#Gráfico de la kurtosis y simetria
+
+Valores_kurtosis<- variables$Desempeño      #Selección de la variable desempeño
+kurtosis_value <- round(kurtosis(Valores_kurtosis), 2)  #Calculo de la kurtosis
+cat("El valor de la kurtosis de la variable Desempeño:", kurtosis_value, "\n") #Imprimir el valor de la kurtosis
+
+Kurtosis_data_frame <- data.frame(value = Valores_kurtosis)  #Creación de un data frame para usar en el grafico como X
+
+ggplot(Kurtosis_data_frame, aes(x = value)) +
+  geom_histogram(aes(y = ..density..), bins = 10, fill = "lightblue", color = "black", alpha = 0.7) +
+  geom_density(color = "red", linewidth = 1) +
+  stat_function(fun = dnorm, 
+                args = list(mean = mean(Valores_kurtosis), sd = sd(Valores_kurtosis)), 
+                color = "blue", linetype = "dashed", size = 1) +
+  ggtitle(paste("Histograma sobre la densidad y kurtosis (Kurtosis =", round(kurtosis_value, 2), ")")) +
+  theme_minimal() +
+  theme(text = element_text(family = "serif"))
+
+Valores_simetria <- round(skewness(Valores_kurtosis), 2)
+Simetria_df <- data.frame(
+  Valor = variables$Desempeño,
+  Simetria = Valores_simetria
+)
+
+ggplot(Simetria_df, aes(x = Valor)) +                     #Gráfico de la simetria
+  geom_histogram(aes(y = ..density..), bins = 10, fill = "lightblue", color = "black", alpha = 0.7) +
+  stat_function(fun = dnorm, 
+                args = list(mean = mean(Simetria_df$Valor), sd = sd(Simetria_df$Valor)),
+                color = "blue", linetype = "dashed", linewidth = 1) +
+  ggtitle(paste("Histograma sobre la simetria (Simetria =", Valores_simetria, ")")) +
+  theme_minimal()
 
 
 #Distribución de las variables cualitativas
